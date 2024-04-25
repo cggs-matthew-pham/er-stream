8be05bffa48f6d223f6aa94909a3e5ce6ef3a870
cggs-matthew-pham <matthew.pham@cggs.act.edu.au>
2024-04-25 16:39:33 +1000

    Updated home page tool descriptions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,8 @@
     <p>
         The goal of this project is to create a set of tools that streamline and guide the process of using AI effectively for education. The tools will help teachers and students to generate prompts, feedback, and evaluations for essays and exams. The tools will be designed to be user-friendly and intuitive, allowing users to easily input their data and receive helpful feedback and guidance.
     </p>
-
+    <h2>AI Essay Feedback Tool</h2>
+    <p>The purpose of this tool is to guide students on effective prompts specifically for getting feedback on essay drafts. Rather than using AI to plagiarise content, students can use this to develop habits for getting immediate feedback, aiming to replicate what a teacher would do (if only they had the time to do so!)</p>
+<h2>AI Exam Feedback Tool</h2>
+<p>The purpose of this tool is to semi-automate the process of teachers allocating feedback and marks, when grading student papers. Students are encouraged to use this tool for formative exams. </p>
 </main>
